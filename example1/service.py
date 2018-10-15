0c1fa73009cbd71c988a731290cfacf306fd563d
Vinicius Pacheco <vfpweb@gmail.com>
2018-10-15 19:12:36 -0300

    example done

--- example1/service.py
+++ example1/service.py
@@ -1,14 +1,11 @@
 import json
 import os
 
-from ast import literal_eval
 from time import sleep
 
 from nameko.web.handlers import http
-from redis import Redis
 
 CONFIG = {'AMQP_URI': os.environ.get('QUEUE_HOST')}
-redis = Redis(host=os.environ.get('REDIS_HOST'), port=6379, db=0)
 
 
 class ApiService:
@@ -18,17 +15,15 @@ class ApiService:
     @http('GET', '/rock/<int:id>')
     def get(self, request, id):
         content_type = {'Content-Type': 'application/json'}
-        data = {'id': id}
+        response = {'id': id}
         try:
             d1 = Domain1()
-            data['first_name'] = d1.task(id)
+            response['first_name'] = d1.task(id)
             d2 = Domain2()
-            data['last_name'] = d2.task(id)
+            response['last_name'] = d2.task(id)
             d3 = Domain3()
-            d3.task(data)
-            uid = 'ex1_{}'.format(id)
-            response = redis.get(uid).decode('utf-8')
-            return 200, content_type, json.dumps(literal_eval(response))
+            response['twitter'] = d3.task(id)
+            return 200, content_type, json.dumps(response)
         except Exception as e:
             return 500, content_type, json.dumps({'error': e})
 
@@ -53,11 +48,9 @@ class Domain2:
 
 class Domain3:
 
-    def task(self, data):
+    def task(self, id):
         sleep(0.5)
-        if data['id'] == 1:
-            data['twitter'] = '@viniciuspach'
+        if id == 1:
+            return '@viniciuspach'
         else:
-            data['twitter'] = '@nose'
-        uid = 'ex1_{}'.format(data['id'])
-        redis.set(uid, data)
+            return '@nose'
--- example2/sync3/service.py
+++ example2/sync3/service.py
@@ -1,22 +1,15 @@
-import os
-
 from time import sleep
 
 from nameko.rpc import rpc
-from redis import Redis
-
-redis = Redis(host=os.environ.get('REDIS_HOST'), port=6379, db=0)
 
 
 class Domain3:
     name = 'ex2_domain3'
 
     @rpc
-    def task(self, data):
+    def task(self, id):
         sleep(0.5)
-        if data['id'] == 1:
-            data['twitter'] = '@viniciuspach'
+        if id == 1:
+            return '@viniciuspach'
         else:
-            data['twitter'] = '@nose'
-        uid = 'ex2_{}'.format(data['id'])
-        redis.set(uid, data)
+            return '@nose'
